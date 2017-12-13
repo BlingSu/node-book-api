0f28393e175a1d79c79e3053cfcb12137660fde0
angelasubi <21978301@qq.com>
2017-12-13 16:58:05 +0800

    fix: app. direction

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 
-app.get('/books/', require('./routes/books'))
+app.use('/books/', require('./routes/books'))
 
 app.listen(3333, () => {
   console.log(`the server is running prot: 3333.....`)
