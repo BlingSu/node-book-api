627bc079c4dd5fb81f0b96f98249b802060a35e3
angelasubi <21978301@qq.com>
2018-03-21 20:05:07 +0800

    fix: cors

--- app.js
+++ app.js
@@ -5,6 +5,19 @@ const app = express()
 app.use('/books/', require('./routes/books'))
 app.use('/user/',require('./routes/user'))
 
+app.all('*',function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild')
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS')
+
+  if (req.method == 'OPTIONS') {
+    res.send(200)
+  }
+  else {
+    next()
+  }
+})
+
 app.listen(3333, () => {
   console.log(`the server is running prot: 3333.....`)
 })
