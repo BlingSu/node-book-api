8112ec051b376e9ef75a1e76e066e85194a678ef
angelasubi <21978301@qq.com>
2018-03-22 09:07:48 +0800

    fix: 跨域

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 const express = require('express')
 const app = express()
 
-
-app.use('/books/', require('./routes/books'))
-app.use('/user/',require('./routes/user'))
-
 app.all('*',function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild')
@@ -17,6 +13,10 @@ app.all('*',function (req, res, next) {
     next()
   }
 })
+
+app.use('/books/', require('./routes/books'))
+app.use('/user/',require('./routes/user'))
+
 
 app.listen(3333, () => {
   console.log(`the server is running prot: 3333.....`)
